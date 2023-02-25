c7e6a6c902d7cd409b397ea4071e4c5933c45043
APC\Andrey <belousovandrey3105@gmail.com>
2023-02-25 12:49:15 +0300

    element_print_branch add generator

--- tests/elements_test.py
+++ tests/elements_test.py
@@ -8,11 +8,10 @@ class TestElements:
         def test_text_box(self, driver):
             text_box_page = TextBoxPage(driver, 'https://demoqa.com/text-box')
             text_box_page.open()
-            text_box_page.fill_all_fields()
-            output_name, output_email, output_cur_address, output_per_address = text_box_page.check_filled_form()
-            print()
-            print(output_name)
-            print(output_email)
-            print(output_cur_address)
-            print(output_per_address)
+            full_name, email, current_address, permanent_address = text_box_page.fill_all_fields()
+            output_name, output_email, output_cur_addr, output_per_addr = text_box_page.check_filled_form()
+            assert full_name == output_name, 'the ful name does not match'
+            assert email == output_email, 'the email does not match'
+            assert current_address == output_cur_addr, 'the current_address does not match'
+            assert permanent_address == output_per_addr, 'the permanent_address does not match'
             # time.sleep(10)
