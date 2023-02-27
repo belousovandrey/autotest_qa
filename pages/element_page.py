f5ae9c6747e194ccecc895d49516bbd8b55cb418
APC\Andrey <belousovandrey3105@gmail.com>
2023-02-27 11:54:25 +0300

    add radiobutton

--- pages/element_page.py
+++ pages/element_page.py
@@ -66,12 +66,13 @@ class CheckBoxPage(BasePage):
 
 
 class RadioButtonPage(BasePage):
-
-    locators = RadioButtonPageLocators
+    locators = RadioButtonPageLocators()
 
     def click_on_the_radio_button(self, choice):
-        choice = {'yes': ?,
-        'impressive':, 'no': ,}
+        choices = {'yes': self.locators.YES_RADIOBUTTON,
+                   'impressive': self.locators.IMPRESSIVE_RADIOBUTTON,
+                   'no': self.locators.NO_RADIOBUTTON}
+        self.element_is_visible(choices[choice]).click()
 
-        time.sleep()
-
+    def get_output_result(self):
+        return self.element_is_present(self.locators.OUTPUT_RESULT).text
