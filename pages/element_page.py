c7cebecc389a004adc4ca523e579a2d9e507598a
APC\Andrey <belousovandrey3105@gmail.com>
2023-03-02 16:28:48 +0300

    UploadAndDownload

--- pages/element_page.py
+++ pages/element_page.py
@@ -1,11 +1,14 @@
+import base64
+import os
 import random
+import time
 
 import requests
 from selenium.webdriver.common.by import By
 
-from generator.generator import generated_person
+from generator.generator import generated_person, generated_file
 from locators.elements_page_locators import TextBoxPageLocators, CheckBoxPageLocators, RadioButtonPageLocators, \
-    WebTablePageLocators, ButtonsPageLocators, LinksPageLocators
+    WebTablePageLocators, ButtonsPageLocators, LinksPageLocators, UploadAndDownloadPageLocators
 from pages.base_page import BasePage
 
 
@@ -183,9 +186,32 @@ class LinksPage(BasePage):
         else:
             return link_href, request.status_code
 
-    def check_broken_link(self,url):
+    def check_broken_link(self, url):
         request = requests.get(url)
         if request.status_code == 200:
             self.element_is_present(self.locators.BAD_REQUEST).click()
         else:
             return request.status_code
+
+
+class UploadAndDownloadPage(BasePage):
+    locators = UploadAndDownloadPageLocators()
+
+    def upload_file(self):
+        file_name, path = generated_file()
+        self.element_is_present(self.locators.UPLOAD_FILE).send_keys(path)
+        os.remove(path)
+        text = self.element_is_present(self.locators.UPLOADED_RESULT).text
+        return file_name.split('\\')[-1], text.split('\\')[-1]
+
+    def download_file(self):
+        link = self.element_is_present(self.locators.DOWNLOAD_FILE).get_attribute('href')
+        link_b = base64.b64decode(link)
+        path_name_file = rf'E:\automation_qa_course\filetest{random.randint(0, 999)}.jpg'
+        with open(path_name_file, 'wb+') as f:
+            offset = link_b.find(b'\xff\xd8')
+            f.write(link_b[offset:])
+            check_file = os.path.exists(path_name_file)
+            f.close()
+        os.remove(path_name_file)
+        return check_file
--- tests/elements_test.py
+++ tests/elements_test.py
@@ -1,6 +1,7 @@
 import random
 
-from pages.element_page import TextBoxPage, CheckBoxPage, RadioButtonPage, WebTablePage, ButtonPage, LinksPage
+from pages.element_page import TextBoxPage, CheckBoxPage, RadioButtonPage, WebTablePage, ButtonPage, LinksPage, \
+    UploadAndDownloadPage
 
 
 class TestElements:
@@ -108,5 +109,22 @@ class TestElements:
         def test_broken_link(self, driver):
             links_page = LinksPage(driver, "https://demoqa.com/links")
             links_page.open()
-            responce_code = links_page.check_broken_link('https://demoqa.com/bad-request')
-            assert responce_code == 400, "the link works or the status code in son 400"
+            resp_code = links_page.check_broken_link('https://demoqa.com/bad-request')
+            assert resp_code == 400, "the link works or the status code in son 400"
+
+
+    class TestUploadAndDownload:
+
+        def test_upload_file(self,driver):
+            upload_download_page = UploadAndDownloadPage(driver, "https://demoqa.com/upload-download")
+            upload_download_page.open()
+            upload_download_page.upload_file()
+            file_name, result = upload_download_page.upload_file()
+            assert file_name == result, "the file has not been uploaded"
+
+
+        def test_download_file(self,driver):
+            links_page = UploadAndDownloadPage(driver, "https://demoqa.com/upload-download")
+            links_page.open()
+            check = upload_download_page.download_file()
+            assert check is True, "the file has not been downloaded"
