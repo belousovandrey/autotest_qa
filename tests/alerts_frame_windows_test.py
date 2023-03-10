9c58158feacedca35ed95891ba56e910ce07695a
APC\Andrey <belousovandrey3105@gmail.com>
2023-03-10 16:24:55 +0300

    add AlertPage

--- tests/alerts_frame_windows_test.py
+++ tests/alerts_frame_windows_test.py
@@ -1,6 +1,6 @@
 import time
 
-from pages.alerts_frame_windows_page import BrowserWindowsPage
+from pages.alerts_frame_windows_page import BrowserWindowsPage, AlertsPage
 
 
 class TestAlertsFrameWindow:
@@ -17,3 +17,29 @@ class TestAlertsFrameWindow:
             new_window_page.open()
             text_result = new_window_page.check_opened_new_window()
             assert text_result == 'This is a sample page'
+
+    class TestAlertPage:
+
+        def test_see_alert(self, driver):
+            alert_page = AlertsPage(driver, 'https://demoqa.com/alerts')
+            alert_page.open()
+            alert_text = alert_page.check_see_alert()
+            assert alert_text == 'You clicked a button'
+
+        def test_alert_appear_5_sec(self, driver):
+            alert_page = AlertsPage(driver, 'https://demoqa.com/alerts')
+            alert_page.open()
+            alert_text = alert_page.check_alert_appear_5_sec()
+            assert alert_text == 'This alert appeared after 5 seconds'
+
+        def test_confirm_alert(self, driver):
+            alert_page = AlertsPage(driver, 'https://demoqa.com/alerts')
+            alert_page.open()
+            alert_text = alert_page.check_confirm_alert()
+            assert alert_text == 'You selected Ok'
+
+        def test_prompt_alert(self, driver):
+            alert_page = AlertsPage(driver, 'https://demoqa.com/alerts')
+            alert_page.open()
+            text, alert_text = alert_page.check_prompt_alert()
+            assert alert_text == f'You entered {text}'
